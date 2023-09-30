211562c1b9effa5013e005ec4b38bbfca6294171
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-09-30 14:39:04 +0000

    Deploying to gh-pages from @ Apopkov1996/goit-js-hw-10@7c8645e8a7bbc575fa1670248e85b1ac6283e04e 🚀

--- index.2ab4401b.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".breed-select"),document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info")][0].addEventListener("click",(function(){}));
-//# sourceMappingURL=index.2ab4401b.js.map
--- index.7a47657c.js
+++
@@ -1,2 +0,0 @@
-({selectEl:document.querySelector(".breed-select"),textLoadEl:document.querySelector(".loader"),textErrorEl:document.querySelector(".error"),divEl:document.querySelector(".cat-info")}).selectEl.addEventListener("click",(function(){}));
-//# sourceMappingURL=index.7a47657c.js.map
