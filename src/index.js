7c6a02c7ef417012a8ae227ab7d1f28a2f44af4f
Apopkov1996 <antonpopkov113@gmail.com>
2023-09-30 19:07:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 
-// fetchBreeds().then(console.log)
-// fetchCatByBreed('chee').then(console.log)
-
 const refs = {
     selectEl: document.querySelector('.breed-select'),
     textLoadEl: document.querySelector('.loader'),
@@ -10,11 +7,23 @@ const refs = {
     divEl: document.querySelector('.cat-info'),
 };
 
-refs.selectEl.addEventListener('change', onSelectElClick);
+refs.textLoadEl.classList.add('is-hidden');
+refs.textErrorEl.classList.add('is-hidden')
 
-function onSelectElClick() {
-    
+refs.selectEl.addEventListener('change', onSelectElChange);
+
+function onSelectElChange(e) {
+    refs.textLoadEl.classList.remove('is-hidden');
+
+    const catId = refs.selectEl.value
+    fetchCatByBreed(catId).then(data => {
+        refs.textLoadEl.classList.add('is-hidden');
+        renderPage(data)
+    }).catch(error => {
+        refs.textErrorEl.classList.remove('is-hidden')
+    })  
 };
+
 
 fetchBreeds().then(data => {
     const markup = data.map(el => {
@@ -22,6 +31,21 @@ fetchBreeds().then(data => {
     }).join('');
 
     refs.selectEl.innerHTML = markup;
-})
+}).catch(error => {
+    refs.textErrorEl.classList.remove('is-hidden')
+});
+
+function renderPage(data) {
+    const markup =  `
+        <h2 class="header-cat-info">${data[0].breeds[0].name}</h2>
+        <img class="img-cat-info" src="${data[0].url}" alt="cat" width = "400">
+        <p class="text-cat-info">${data[0].breeds[0].description}</p>
+        <p class="text-cat-info">${data[0].breeds[0].temperament}</p>
+        `
+    refs.divEl.innerHTML = markup;
+}
+
+
+
 
 
